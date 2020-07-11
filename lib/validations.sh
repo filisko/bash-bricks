e14e11e4d73a85bd6720c314eacdd4e007049d40
Filis Futsarov <filisfutsarov@gmail.com>
2020-07-11 14:48:58 +0200

    update

--- lib/validations.sh
+++ lib/validations.sh
@@ -16,10 +16,7 @@ valid_date() {
         return 1
     fi
 
-    # declare -A max_day_by_month
-    # max_day_by_month=([1]=31 [2]=29 [3]=31 [4]=30 [5]=31 [6]=30 [7]=31 [8]=31 [9]=30 [10]=31 [11]=30 [12]=31)
-
-    declare -a max_day_by_month=("jack" "paco" "javier")
+    declare -a max_day_by_month=([1]=31 [2]=29 [3]=31 [4]=30 [5]=31 [6]=30 [7]=31 [8]=31 [9]=30 [10]=31 [11]=30 [12]=31)
 
     max_day=${max_day_by_month[month]}
     
