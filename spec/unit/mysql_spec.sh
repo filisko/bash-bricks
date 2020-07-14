eb89dddc25e10416a50fa0932021fc7c92622dce
Filis Futsarov <filisfutsarov@gmail.com>
2020-07-14 19:30:02 +0200

    update

--- spec/unit/mysql_spec.sh
+++ spec/unit/mysql_spec.sh
@@ -6,14 +6,14 @@ Describe 'bb_mysql_csv'
   It 'returns first number of http status code when doesnt start with 2'
     bb_mysql_raw() {
       cat <<EOL
-1	random text!123 ñ,."	01234
+1	random text!123 "ñ,.	01234
 2	text	56789
 EOL
     }
 
     When call bb_mysql_csv "SELECT * FROM users"
     The output should equal "$(cat <<EOL
-"1","random text!123 ñ,.""","01234"
+"1","random text!123 ""ñ,.","01234"
 "2","text","56789"
 EOL
 )"
