7f20f74f93925c9643d538e0787d2bbaf91de2d2
Filis Futsarov <myumyunov@auxmoney.com>
2020-07-15 22:30:35 +0200

    readd csv

--- spec/strings_spec.sh
+++ spec/strings_spec.sh
@@ -1,3 +1,29 @@
 #https://github.com/shellspec/shellspec/blob/master/docs/references.md
 
 Include lib/strings.sh
+
+Describe 'variables'
+    data() {
+      set
+    }
+    Data data
+
+    Example "default"
+        myvar=test123
+        When call variables
+        The output should include "myvar"
+    End
+End
+
+Describe 'flatten'
+    data() {
+      set
+    }
+    Data data
+
+    Example "default"
+        this=test123
+        When call flatten "replace {{this}}"
+        The output should equal "replace test123"
+    End
+End
