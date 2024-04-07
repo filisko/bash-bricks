a4e38155d018f558d638a299a95c540146c00124
Filis Futsarov <filisfutsarov@gmail.com>
2024-04-07 19:01:20 +0200

    update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+url="https://github.com/filisko/bash-bricks/archive/refs/heads/master.zip"
+
+git pull
