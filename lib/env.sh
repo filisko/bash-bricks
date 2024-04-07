f813bd6e000930dd91990acce86287806df5afbe
Filis Futsarov <filisfutsarov@gmail.com>
2024-04-07 18:27:21 +0200

    load env

--- lib/env.sh
+++ lib/env.sh
@@ -4,11 +4,13 @@ load_env() {
     local path="$1"
 
     if [ -f "$path" ]; then
-        num_lines=$(cat .env | sed 's/#.*//g')
+        return 1;
+    fi
+    
+    num_lines=$(cat "$path" | sed 's/#.*//g')
 
-        if [ ! -z "$num_lines" ]; then
-            export $(echo -n "$num_lines" | xargs)
-        fi
+    if [ ! -z "$num_lines" ]; then
+        export $(echo -n "$num_lines" | xargs)
     fi
 }
 
